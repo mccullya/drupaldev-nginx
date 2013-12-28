552eca3fb26b7e35f0b674468acbce494321768d
Mike Bell <hello@mikebell.io>
2013-12-28 21:54:30 +0000

    Temp revmoed compass until I can figure out installing it as a gemset using the rvm module

--- manifests/default.pp
+++ manifests/default.pp
@@ -83,17 +83,17 @@ php::pear::module { 'Console_Table':
   use_package => 'no',
 }
 
-class { 'ruby':
-  gems_version  => 'latest'
-}
-
-package { [
-  'compass',
-]:
-  provider => 'gem',
-  ensure   => 'installed',
-  require  => Package[[rubygems]]
-}
+#class { 'ruby':
+#  gems_version  => 'latest'
+#}
+#
+#package { [
+#  'compass',
+#]:
+#  provider => 'gem',
+#  ensure   => 'installed',
+#  require  => Package[[rubygems]]
+#}
 
 class { 'mailcatcher': }
 
