8f79e4f8f14247fdb2550f795318f74e575d78c3
Mike Bell <hello@mikebell.io>
2014-01-13 22:59:52 +0000

    Added defines for nginx vhost and mysql db

--- manifests/default.pp
+++ manifests/default.pp
@@ -114,9 +114,78 @@ class { 'mailcatcher': }
 class { 'xhprof': }
 
 if $nginx_values == undef {
-  $nginx_values = hiera('nginx', false)
+  $nginx_values = hiera('sites', false)
 }
 
 if count($nginx_values['vhosts']) > 0 {
-create_resources(nginx_vhost, $nginx_values['vhosts'])
+  create_resources(nginx_vhost, $nginx_values['vhosts'])
+}
+
+define nginx_vhost (
+  $server_name,
+  $server_aliases = [],
+  $www_root,
+  $listen_port,
+  $index_files,
+  $envvars = [],
+  ){
+  $merged_server_name = concat([$server_name], $server_aliases)
+
+  if is_array($index_files) and count($index_files) > 0 {
+    $try_files = $index_files[count($index_files) - 1]
+  } else {
+    $try_files = 'index.php'
+  }
+
+  nginx::resource::vhost { $server_name:
+    server_name => $merged_server_name,
+    www_root    => $www_root,
+    listen_port => $listen_port,
+    index_files => $index_files,
+    try_files   => ['$uri', '$uri/', "/${try_files}?\$args"],
+  }
+
+  $fastcgi_param = concat(
+    [
+      'PATH_INFO $fastcgi_path_info',
+      'PATH_TRANSLATED $document_root$fastcgi_path_info',
+      'SCRIPT_FILENAME $document_root$fastcgi_script_name',
+    ], $envvars)
+
+  nginx::resource::location { "${server_name}-php":
+    ensure              => present,
+    vhost               => $server_name,
+    location            => '~ \.php$',
+    proxy               => undef,
+    try_files           => ['$uri', '$uri/', "/${try_files}?\$args"],
+    www_root            => $www_root,
+    location_cfg_append => {
+      'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+      'fastcgi_param'           => $fastcgi_param,
+      'fastcgi_pass'            => $fastcgi_pass,
+      'fastcgi_index'           => 'index.php',
+      'include'                 => 'fastcgi_params'
+    },
+    notify              => Class['nginx::service'],
+  }
+}
+
+define mysql_db (
+  $user,
+  $password,
+  $host,
+  $grant    = [],
+  $sql_file = false
+  ) {
+    if $name == '' or $password == '' or $host == '' {
+    fail( 'MySQL DB requires that name, password and host be set. Please check your settings!' )
+  }
+
+  mysql::db { $name:
+    user     => $user,
+    password => $password,
+    host     => $host,
+    grant    => $grant,
+    sql      => $sql_file,
+  }
 }
