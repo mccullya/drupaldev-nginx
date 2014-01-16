6a36a96309c24bd2e7fdb45b23c4399dc181884f
Mike Bell <hello@mikebell.io>
2014-01-16 17:50:18 +0000

    Temporary fix for fastcgi_params

--- manifests/default.pp
+++ manifests/default.pp
@@ -161,7 +161,10 @@ define nginx_vhost (
     www_root            => $www_root,
     location_cfg_append => {
       'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
-      'fastcgi_param'           => $fastcgi_param,
+      #'fastcgi_param'           => $fastcgi_param,
+      'fastcgi_param'           => 'PATH_INFO $fastcgi_path_info',
+      'fastcgi_param'           => 'PATH_TRANSLATED $document_root$fastcgi_path_info',
+      'fastcgi_param'           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
       'fastcgi_pass'            => $fastcgi_pass,
       'fastcgi_index'           => 'index.php',
       'include'                 => 'fastcgi_params'
