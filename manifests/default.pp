5121cd674289253924779d8d3ca6379fba06174a
Mike Bell <hello@mikebell.io>
2014-01-16 22:44:49 +0000

    fix for fastcgi_param being very lame

--- manifests/default.pp
+++ manifests/default.pp
@@ -163,9 +163,9 @@ define nginx_vhost (
       'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
       #'fastcgi_param'           => $fastcgi_param,
       'fastcgi_param'           => 'PATH_INFO $fastcgi_path_info',
-      'fastcgi_param'           => 'PATH_TRANSLATED $document_root$fastcgi_path_info',
-      'fastcgi_param'           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
-      'fastcgi_pass'            => $fastcgi_pass,
+      'fastcgi_param '           => 'PATH_TRANSLATED $document_root$fastcgi_path_info',
+      'fastcgi_param  '           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
+#      'fastcgi_pass'            => $fastcgi_pass,
       'fastcgi_index'           => 'index.php',
       'include'                 => 'fastcgi_params'
     },
